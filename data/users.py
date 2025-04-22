42df79abe3673ce7d575af6acef84bf16c4647a1
MarraQ <naplishka@gmail.com>
2025-04-22 23:15:46 +0300

    silly commit

--- data/users.py
+++ data/users.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+
 import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
@@ -11,3 +13,9 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
     hpasw = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True, default=datetime.now())
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
