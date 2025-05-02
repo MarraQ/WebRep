9a675c14b92edda1c7708e06dc8cd840f2096c46
MarraQ <naplishka@gmail.com>
2025-05-02 21:43:58 +0300

    brainrot commit

---
+++ data/ads.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Ad(SqlAlchemyBase):
+    __tablename__ = "ads"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True, default=datetime.now())
+    owner_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship("User")
